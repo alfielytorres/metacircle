c625229daab7ae2193a7c006019466d5fe79af45
Alfonso Torres <alfietorres@Alfonsos-MacBook-Pro.local>
2022-01-06 09:24:04 +1100

    fixed it

--- src/NavBar.js
+++ src/NavBar.js
@@ -211,16 +211,31 @@ function NavBar() {
                       
                     </li>
                     <li>
+
+                    {currentUser ?
+                    
+                    <button
+                    onClick={handleLogout}
+  
+                    className="group relative w-full flex justify-center py-3 px-3 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    aria-label="Log out"
+                    title="Log ou"
+                  >
+                    Logout
+                  </button> :
                       <a
                         
                         href="/"
-                        hidden={loading || currentUser}
+              
                         className="group relative w-full flex justify-center py-3 px-3 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                         aria-label="Get Started"
                         title="Get Started"
                       >
                         Get Started
                       </a>
+
+
+                    }
 
 
                     
